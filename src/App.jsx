5ce063306813bd07bd6f078d7cac5c0d60ad3d76
Fasilo03 <fasilalemye3@gmail.com>
2025-10-22 09:12:42 +0200

    some existing code of reac fix

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./Main.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <nav className="nav-container">
+        <img className="nav-logo" src="/favicon.ico" alt="React logo" />
+        <h3 className="nav-title">ReactFacts</h3>
+
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
