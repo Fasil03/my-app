1bbffd98c5e6230b15ddbb26e12f8f83b80d50bb
Fasilo03 <fasilalemye3@gmail.com>
2025-10-22 14:47:02 +0200

    Normalize line endings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ function Navbar() {
       <nav className="nav-container">
         <img className="nav-logo" src="/favicon.ico" alt="React logo" />
         <h3 className="nav-title">ReactFacts</h3>
-
+          <ul className="nav-list">
+            <li>Contact</li>
+            <li>Reference</li>
+          </ul>
       </nav>
     </header>
   );
